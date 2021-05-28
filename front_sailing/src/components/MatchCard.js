df041086d3be2acf85fd8e26f6fbb6a60a736097
Sebastien Cotovio <seb95370@gmail.com>
2021-05-28 20:14:19 +0200

    pull main

--- front_sailing/src/components/MatchCard.js
+++ front_sailing/src/components/MatchCard.js
@@ -3,18 +3,20 @@ import axios from "axios";
 
 function MatchCard(props) {
     const { match } = props;
-    const data ='';
+    let[data, setData] = useState([]);
 
-        axios.get(`/api/teamName/${match.id}`).then((res) => {const data = res.data ; console.log(data);});
+    useEffect(() => {
+        axios.get(`/api/teamName/${match.id}`).then((res) => setData(res.data));
+        console.log(data);
+    }, []);
 
 
     return (
         <div>
             <div class="div-game-info"> "{match.game_name}" | Mise : {match.mise} SC</div>
             <div class="div-match-0">
-                <div class="div-match-1"> 
-                {data.team_name}
-                
+                <div class="div-match-1">
+                    {data.team_name}
                 </div>
                 <div class="div-match-2">
                     VS
